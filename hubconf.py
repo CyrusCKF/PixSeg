f23ea1088df13b3044c7d289790bc8276fce8d5c
Cyrus C <97295877+CyrusCKF@users.noreply.github.com>
2025-02-27 15:17:55 +0800

    test hubconf

---
+++ hubconf.py
@@ -0,0 +1,7 @@
+dependencies = ["torch"]
+from src.semantic_segmentation_toolkit.models.fcn import (
+    fcn_mobilenet_v3_large,
+    fcn_resnet34,
+    fcn_vgg16,
+)
+from src.semantic_segmentation_toolkit.models.pspnet import pspnet_resnet50
